c30e31788ee93bde0ed5902f1522206333a23eee
Amir Hossain Limon <146566739+Amirhossainlimon@users.noreply.github.com>
2025-11-06 16:09:09 +0600

    Add files via upload
    use of post and pre increment operators.

---
+++ use_of_post_and_pre.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1, x = 1;
+
+    cout << "Post-increment: " << i++ << endl;
+    cout << "Pre-increment: " << ++x << endl;
+
+    return 0;
+}
+
